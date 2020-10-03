792aae6fd60584467fb6747fd4fcdce78d1b47a5
Andrew Barlow <dandrewbarlow@gmail.com>
2020-10-03 03:05:34 -0600

    switched to class based implementation, added requirements file

--- Makefile
+++ Makefile
@@ -1,6 +1,4 @@
 main:
 	python3 solow.py
-
-test:
-	./solow.py 1 1 1
-	./solow.py string string string
+install:
+	pip install -r requirements.txt
--- solow.py
+++ solow.py
@@ -7,36 +7,43 @@
 import pyinputplus as pyin
 import sys
 
+# Define a class for the Solow model
+class solow:
+    def __init__(self, labor, productivity, saveRate, depreciationRate):
+        self.labor = labor
+        self.productivity = productivity
+        self.saveRate = saveRate
+        self.depreciationRate = depreciationRate
+
+    # Solving for Capital
+    def solveK(self):
+        numerator = self.saveRate * self.productivity
+        return self.labor * pow( (numerator / self.depreciationRate), (3/2) )
+
+    # Solving for Production
+    def solveY(self):
+        part1 = pow( self.productivity, (3/2) )
+        part2 = pow( (self.saveRate * self.productivity) / self.depreciationRate, (1/2) )
+        return part1 * part2 * self.labor
+    
+    # Solving for Investment
+    def solveI(self):
+        part1 = self.saveRate * pow(self.productivity, (3/2) )
+        part2 = pow( ( (self.saveRate * self.productivity) / self.depreciationRate), (1/2) )
+        return part1 * part2 * self.labor
+
+    # Solving for Consumption
+    def solveC(self):
+        part1 = (1 - self.saveRate) * pow(self.productivity, (3/2))
+        part2 = pow( ((self.saveRate * self.productivity) / self.depreciationRate), (1/2) )
+        return part1 * part2 * self.labor
+
 # Print a usage statement on error
 def usage():
     print("USAGE:")
     print("./solow.py $1 $2 $3 $4  with number arguments")
     print("OR")
     print("./solow.py to enter the cli")
-
-
-# Solving for Capital
-def solveK(labor, productivity, saveRate, depreciationRate):
-    numerator = saveRate * productivity
-    return labor * pow( (numerator / depreciationRate), (3/2) )
-
-# Solving for Production
-def solveY(labor, productivity, saveRate, depreciationRate):
-    part1 = pow( productivity, (3/2) )
-    part2 = pow( (saveRate * productivity) / depreciationRate, (1/2) )
-    return part1 * part2 * labor
-
-# Solving for Investment
-def solveI(labor, productivity, saveRate, depreciationRate):
-    part1 = saveRate * pow(productivity, (3/2) )
-    part2 = pow( ( (saveRate * productivity) / depreciationRate), (1/2) )
-    return part1 * part2 * labor
-
-# Solving for Consumption
-def solveC(labor, productivity, saveRate, depreciationRate):
-    part1 = (1 - saveRate) * pow(productivity, (3/2))
-    part2 = pow( ((saveRate * productivity) / depreciationRate), (1/2) )
-    return part1 * part2 * labor
 
 # Get input for which variable to solve for
 def getInput():
@@ -68,15 +75,15 @@ def cli():
         print("Enter the variable to be solved (K, Y, I, C)")
         process = getInput()
         params = getParams()
+        result = solow( params["labor"], params["productivity"], params["saveRate"], params["depreciationRate"] )
         if (process == "K"):
-            result = solveK( params["labor"], params["productivity"], params["saveRate"], params["depreciationRate"] )
+            print( result.solveK() )
         if (process == "Y"):
-            result = solveY( params["labor"], params["productivity"], params["saveRate"], params["depreciationRate"] )
+            print( result.solveY() )
         if (process == "I"):
-            result = solveI( params["labor"], params["productivity"], params["saveRate"], params["depreciationRate"] )
+            print( result.solveI() )
         if (process == "C"):
-            result = solveC( params["labor"], params["productivity"], params["saveRate"], params["depreciationRate"] )
-        print( result )
+            print( result.solveC() )
 
 
 def main():
